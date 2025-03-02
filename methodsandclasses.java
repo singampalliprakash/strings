7d9329a2c55a2daaf83d1c0dfd72113e187ccbce
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-03-02 20:04:36 +0530

    methodsandclasses.java

---
+++ methodsandclasses.java
@@ -0,0 +1,31 @@
+
+//methods and classes
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("main");
+		abc.hurey();
+		my.hey();
+		dar.hurey();
+	}
+	class abc{
+	    public static void hurey(){
+	        System.out.println("what are you doing");
+	    }
+	}
+	class my{
+	    public static void hey(){
+	        System.out.println("what's up");
+	    }
+	}
+	class dar{
+	    public static void hurey(){
+	        System.out.println("nice to meeting you");
+	    }
+	}
+	
+	
+	
+	
+	
+}
